730e944e8b3a27fe54c20647eb82e065131de175
Despresso <118217896+Despresso16@users.noreply.github.com>
2023-11-17 11:31:50 +0100

    Zadania z obiektow
    do dokonczenia

---
+++ obiekty/obiekty.js
@@ -0,0 +1,21 @@
+// zad1
+const osoba = {
+    imie: "Jan",
+    nazwisko: "Kowalski",
+}
+console.log(osoba.imie + " " + osoba.nazwisko);
+//zad2
+console.log("zad2");
+const ksiazka = {
+    tytul: "Hobbit",
+    autor: "Tolkien",
+    wydawnictwo: "Muza",
+}
+console.log("Obiekt: " + ksiazka.tytul + ", " + ksiazka.autor + ", " + ksiazka.wydawnictwo);
+ksiazka.tytul = ["Moj plan"];
+ksiazka.autor = ["Adam Hilter"];
+ksiazka.wydawnictwo = ["Czadowo"];
+console.log("Tabela: " + ksiazka.tytul + ", " + ksiazka.autor + ", " + ksiazka.wydawnictwo);
+//zad3
+
+
